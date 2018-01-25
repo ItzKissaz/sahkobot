2f527a7b2865696ac55c2460427eb46a940c4485
RaikasCoding <fanipostiraikas@gmail.com>
2018-01-25 19:00:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const commando = require('discord.js-commando');
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
+bot.registery.registerGroup('random', 'Random');
+bot.registery.registerCommandsIn(__dirname + "/commands");
 client.on('message', message => {
     if (message.content === ':stats') {
     	message.
