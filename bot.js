15b8cf8b183fbbf5fd4bda57df6411be7df409f9
RaikasCoding <fanipostiraikas@gmail.com>
2018-01-27 15:12:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,8 +12,8 @@ bot.on('message', (message) => {
 bot.on('message', (message) => {
     if(message.content == 'Cbot') {
         message.channel.sendMessage('**Chocolate Bot __Version 0.1__');
-        }
         message.channel.sendMessage('__This **Discord Bot** Using Discord.NET. Bots host is **Heroku**__');
-});
+    }
+        });
 
 bot.login('NDA1MDM5MjkyODcyNjU0ODU4.DU3IVw.yph7a4t9m4AwP7Q3u71HcnUPe4Y');
