99eea4b7d0ad26a79baf8b85ba6f5f6a6c903857
RaikasCoding <fanipostiraikas@gmail.com>
2018-01-27 10:30:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-client.on('message', (message) => {
+bot.on('message', (message) => {
     if(message.content == 'Chelp') {
-        message.
-        reply('**Chocolate Bot By __RaikaZ#0178__**');
+        message.channel.sendMessage('**Chocolate Bot By __RaikaZ#0178__**');
         }
 });
 
 
-client.login('NDA1MDM5MjkyODcyNjU0ODU4.DU3IVw.yph7a4t9m4AwP7Q3u71HcnUPe4Y');
+bot.login('NDA1MDM5MjkyODcyNjU0ODU4.DU3IVw.yph7a4t9m4AwP7Q3u71HcnUPe4Y');
