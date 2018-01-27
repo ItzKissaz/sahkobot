5c596387adc089d874577d09ccbca3968321d4bf
RaikasCoding <fanipostiraikas@gmail.com>
2018-01-27 15:30:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const bot = new Discord.Client();
 bot.on('message', (message) => {
     if(message.content == 'Chelp') {
         message.reply('**Chocolate Bot** __Help:__');
+        message.channel.sendMessage('**Cban** = __Bans__ the user ```COMING SOON````');
         message.channel.sendMessage('**Cbot** = Bot info');
         message.channel.sendMessage('**Chocolate Bot By __RaikaZ#0178__**');
         }
@@ -15,12 +16,13 @@ bot.on('message', (message) => {
         message.channel.sendMessage('__This **Discord Bot** Using Discord.NET. Bots host is **Heroku**__');
         message.channel.sendMessage('Help command = **Chelp**')
     }
+});
 bot.on('message', (message) => {
     if(message.content == 'Cban') {
-        message.channel.sendMessage('**ERROR:** __Command not done!__ @RaikaZ#0178 is so sorry!')
-        message.channel.sendFile('sorry.png')
+        message.channel.sendMessage('**ERROR:** __Command not done!__ @RaikaZ#0178 is so sorry');
+        message.channel.sendFile('sorry.png');
     }
-}
-        });
+});
+        
 
 bot.login('NDA1MDM5MjkyODcyNjU0ODU4.DU3IVw.yph7a4t9m4AwP7Q3u71HcnUPe4Y');
