433bdfacb8ea4cffd6711fcc86e1c06e6995da38
RaikasCoding <fanipostiraikas@gmail.com>
2018-01-31 18:43:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,6 +21,12 @@ bot.on('message', (message) => {
     if(message.content == 'c!admin test') {
         message.channel.sendMessage('Check your **Direct Messages**')
         message.author.sendMessage("Working :)")
+    if(message.content == 'c!admin tinder') {
+        let age = args[0]; // Remember arrays are 0-based!.
+        let sex = args[1];
+        let location = args[2];
+        message.reply(`Hello ${message.author.username}, I see you're a ${age} year old ${sex} from ${location}. Wanna date?`);
+}
     }
 });
         
