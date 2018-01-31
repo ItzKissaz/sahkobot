ac7d41f8b44a07a6e99e1ff8a5633550c1e47f2a
RaikasCoding <fanipostiraikas@gmail.com>
2018-01-31 17:51:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,10 +52,10 @@ bot.on("ready", () => {
 });        
 bot.on("ready", () => {
   // This event will run if the bot starts, and logs in, successfully.
-  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  console.log(`Bot has started, with $bot.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  bot.user.setGame(`on ${client.guilds.size} servers`);
+  bot.user.setGame(`on $bot.guilds.size} servers`);
 });
 bot.login('NDA1MDM5MjkyODcyNjU0ODU4.DVJCgQ.eKTu3KpKiccLHmqr9wxl-fyzsaM');
 
