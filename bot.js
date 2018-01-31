e858f78c5b9aa069eb36eb7f0c322f65cd5c0c14
RaikasCoding <fanipostiraikas@gmail.com>
2018-01-31 16:38:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,18 +4,23 @@ const bot = new Discord.Client();
 bot.on('message', (message) => {
     if(message.content == 'c!help') {
         message.reply('**Chocolate Bot** __Help:__');
-        message.channel.sendMessage('**Cban** = __Bans__ the user ```COMING SOON```');
-        message.channel.sendMessage('**Cbot** = Bot info');
+        message.channel.sendMessage('**--c!ban--** = __Bans__ the user ```COMING SOON```');
+        message.channel.sendMessage('**c!bot** = Bot info');
         message.channel.sendMessage('**Chocolate Bot By __RaikaZ#0178__**');
         }
 });
 
 bot.on('message', (message) => {
-    if(message.content == 'Cbot') {
+    if(message.content == 'c!bot') {
         message.channel.sendMessage('**Chocolate Bot** __Version 0.1__');
         message.channel.sendMessage('__This **Discord Bot** Using Discord.NET. Bots host is **Heroku**__');
         message.channel.sendMessage('Help command = **Chelp**')
     }
+});
+bot.on('message', (message) => 
+       if(message.content == 'c!admin test') {
+       message.author.sendMessage("Your message here.")
+       }
 });
 bot.on('message', (message) => {
     if(message.content == 'Cban') {
