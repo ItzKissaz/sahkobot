f97f87ed24e0d4ddcc0754b1b7574b3ab29649d7
RaikasCoding <fanipostiraikas@gmail.com>
2018-01-31 19:29:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,65 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-bot.on('message', (message) => {
-    if(message.content == 'c!help') {
-        message.reply('**Chocolate Bot** __Help:__');
-        message.channel.sendMessage('**--c!ban--** = __Bans__ the user ```COMING SOON```');
-        message.channel.sendMessage('**c!bot** = Bot info');
-        message.channel.sendMessage('**Chocolate Bot By __RaikaZ#0178__**');
-        }
-});
-
-bot.on('message', (message) => {
-    if(message.content == 'c!bot') {
-        message.channel.sendMessage('**Chocolate Bot** __Version 0.1__');
-        message.channel.sendMessage('__This **Discord Bot** Using Discord.NET. Bots host is **Heroku**__');
-        message.channel.sendMessage('Help command = **Chelp**')
-    }
-});
-bot.on('message', (message) => {
-    if(message.content == 'c!admin test') {
-        message.channel.sendMessage('Check your **Direct Messages**')
-        message.author.sendMessage("Working :)")
-    if(message.content == 'c!admin tinder') {
-        let age = args[0]; // Remember arrays are 0-based!.
-        let sex = args[1];
-        let location = args[2];
-        message.reply('Hello ${message.author.username}, I see you`re a ${age} year old ${sex} from ${location}. Wanna date?');
-}
-    }
-});
-        
-     
-
-bot.on('message', (message) => {
-    if(message.content == 'c!ban') {
-        message.channel.sendMessage('**ERROR:** __Command not done!__ @RaikaZ\#0178  is so sorry');
-        message.channel.sendFile('sorry.png');
-    }
-});
-
-bot.on('message', (message) => {
-    if(message.content == 'c!admin changemessage huoltokatko') {
-        message.channel.sendMessage('Changed the **Playing** message');
-        bot.user.setGame('chocolatebot.tk | FixPause');
-    }
-});
-bot.on('message', (message) => {
-    if(message.content == 'c!admin changemessage normal') {
-        message.channel.sendMessage('Changed the **Playing** message');
-        bot.user.setGame('chocolatebot.tk | c!help ');
-    }
-});
-
-// Set the bot's online/idle/dnd/invisible status
-bot.on("ready", () => {
-    bot.user.setStatus("idle");
-    console.log('Bot is now online');const Discord = require('discord.js');
-const bot = new Discord.Client();
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
 var prefix = "c!";
 
 client.on("ready", () => client.user.setPresence({ status: "idle", game: {name: `chocolatebot.tk | c!help`} }));
