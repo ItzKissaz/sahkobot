05d39fb4abf7afa4806b20069631bf6bc98ca6cf
RaikasCoding <fanipostiraikas@gmail.com>
2018-02-01 15:58:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,7 +29,7 @@ bot.on('message', async message => {
         message.author.sendMessage("Working :)")
     };
     if (message.content.startsWith(prefix + 'version')) {
-         var versio = new.Discord.RichEmbed().setColor(0x0000FF).addField("**Version 0.5**", "**Added**", "New code style.", "**Removed**", "Nothing", "**__Fixed__**", "NothingH.");
+         var versio = new.Discord.RichEmbed().setColor(0x0000FF).addField('**Version 0.5**', '**Added**', 'New code style.', '**Removed**', 'Nothing', '**__Fixed__**', 'Nothing.');
          message.channel.sendEmbed(versio);
      };
     if(message.content.startsWith(prefix + 'admin tinder')) {
