4a3ff3303d89322cb77765810e6a0e42b767a752
RaikaZ <fanipostiraikas@gmail.com>
2018-04-02 20:30:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,35 +18,12 @@ bot.on("guildMemberRemove", (member) => {
 const fs = require("fs");
 
 
-let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+var saldo = JSON.parse(fs.readFileSync("./saldo.json", "utf8"));
 
-
-bot.on("message", message => {
-  if (!message.content.startsWith(prefix)) return;
-  if (message.author.bot) return;
-
-  if (!points[message.author.id]) points[message.author.id] = {
-    points: 0,
-    level: 0
-  };
-  let userData = points[message.author.id];
-  userData.points++;
-
-  let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
-  if (curLevel > userData.level) {
-    // Level up!
-    userData.level = curLevel;
-    message.reply(`You"ve leveled up to level **${curLevel}**! Ain"t that dandy?`);
-  }
-
-  if (message.content.startsWith(prefix + "level")) {
-    message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
-  }
-  fs.writeFile("./points.json", JSON.stringify(points), (err) => {
-    if (err) console.error(err)
-  });
-
-});
+if (userData[sender.id]) userData[sender.id] = {
+    OweqBalance: 0
+    }
+userData[sender.id].OweqBalance++;
 bot.on('message', async message => {
     if(message.content.startsWith(prefix + 'help')) {
         message.delete(1000); //Supposed to delete message
