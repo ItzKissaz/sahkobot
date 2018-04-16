098368525eb490ffcab921f881a91e93b4f386c9
RaikaZ <fanipostiraikas@gmail.com>
2018-04-16 14:23:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var prefix = ",";
 var test = 100;
 var channelid = "435140636631367712";
 var sender = "message.author";
-bot.user.setGame("Moi");
+client.user.setPresence({ game: { name: 'yourGameGoesHere', type: 0 } });
 console.log("Olen valmiina! Joona#1266 auttoi botin rakentamisessa.");
 console.log("Myöskään virheitä ei löytynyt");
 var NewUsers = 0;
