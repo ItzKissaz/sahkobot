88680d1395ed27b8dec184a007f833498010d504
RaikaZ <fanipostiraikas@gmail.com>
2018-04-16 20:40:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,17 @@ bot.on("guildMemberRemove", (member) => {
     bot.channels.get(channelid).sendMessage("Member left.");
         
 });
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
 
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
 
 bot.on('message', async message => {
     if(message.content.startsWith(prefix + 'help')) {
