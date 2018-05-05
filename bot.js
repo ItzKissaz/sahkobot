d3c3a9f7381b83800e4df6e6910d85236b363a14
RaikaZ <fanipostiraikas@gmail.com>
2018-05-05 11:23:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,8 @@ bot.on('message', async message => {
         if (message.content.slice(8, message.content.lenght) <= 2) {
             message.channel.send("Kerro ideasi meille tällä komenolla");
         }else {
-            message.guild.channels.find("name", "bottiideat").sendMessage("blabla");
+            message.guild.channels.find("name", "bottiideat").sendMessage(message.content.slice(8, message.content.lenght)));
+            message.channel.send("Kiitos ideastasi");
            }
     }
     if (message.content.startsWith(prefix + "kick")) {
